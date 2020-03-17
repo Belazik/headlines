11276853d2a6e377b8be37222deb7ecc72b93760
Kotletta-TT <assimilation13cd@gmail.com>
2020-03-17 09:16:06 +0300

    using GET

--- headlines.py
+++ headlines.py
@@ -1,15 +1,22 @@
 import feedparser
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 RSS_FEEDS = {"sochi": "https://sochinews.io/feed/",
              "world": "http://feeds.bbci.co.uk/news/rss.xml",
              "habr": "https://habr.com/ru/rss/best/weekly/?fl=ru"}
 
+
+
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication="sochi"):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "sochi"
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
 
     return render_template("home.html", articles=feed['entries'])
